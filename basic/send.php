c344cb964ac4a967b03c83cf2ac6a2d6bb35d9ad
Tiffany B. Brown <tiffany@webinista.com>
2012-02-24 12:25:26 -0800

    Finishing the onopen function. Re-writing the onmessage function. Adding a comment to explain why onmessage won't work in this example.

---
+++ basic/send.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('../php/lib.php');
+
+$msg = 'It is now: '.date('g:i:sa T',time());
+
+header('Content-Type: text/event-stream');
+
+$i = 0;
+
+while($i < 10){
+   $msg = 'It is now: '.date('g:i:sa T',time());
+   doit($msg);
+   sleep(2);
+   $i++;
+}
